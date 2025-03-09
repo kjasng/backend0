1635b4300b13800451659553cb0f5a63f7cdc912
Kjas Ng <manhnguyenls131@gmail.com>
2025-03-09 20:26:09 +0700

    add dotnet

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express')
+require('dotenv').config()
 
+// define express app
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 8080;
+const hostname = process.env.HOSTNAME;
 
+// config template engine
+app.set('view engine', 'pug')
+
+
+// route destination
 app.get('/', (req, res) => {
-    res.send('Hello World with Kjas Nguyen')
+    res.render('index', { title: 'Hey', message: 'Hello World' })
 })
 
 app.get('/abc', (req, res) => {
-    res.send('Check abc')
+    res.render('abc', { title: 'Hey', message: 'This is abc route!' })
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Server is running on port ${port}`)
 })
 
